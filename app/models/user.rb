82d2c20104adede12b53ee2f32e8380d98dc1aa2
ali.uzair <uzairnns5@gmail.com>
2023-03-27 11:45:30 +0500

    email and name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true, format: { with: /\A[a-zA-Z0-9]+\z/, message: "only allows letters and digits" }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "must be a valid email address" }
   validates :password, presence: true, length: { minimum: 6 }, confirmation: true
   validates :password_confirmation, presence: true
 
